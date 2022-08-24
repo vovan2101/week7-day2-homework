3897bc330eb829c06ea73ccc5bbcbf587469bca1
Vladimir <veliseykin2000@gmail.com>
2022-08-24 07:24:44 -0700

    Create homework.js

---
+++ js/homework.js
@@ -0,0 +1,38 @@
+// exercise 1
+// console.clear()
+
+// function toCelcius(f) {
+//     const1 = 1.8;
+//     const2 = 32;
+//     c = (f - const2) / const1
+//     return c;
+//   };
+// console.log(toCelcius(50))
+// console.log(toCelcius(32))
+// console.log(toCelcius(212))
+
+// exercise 2
+// class user{
+//     constructor(username, password){
+//         this.username = username;
+//         this.password = password;
+//     };
+
+//     changePassword(oldPassword, newPassword){
+//         if (oldPassword === this.password){
+//             this.password = newPassword
+//             return this.password;
+//         } else {
+//             console.log("Old password is incorect, try again")
+//         };
+//     };
+// };
+
+
+
+// let user1 = new user("Vlad", "123");
+// console.log(user1.username);
+// console.log(user1.password);
+// user1.changePassword("sdfdsf", "123123");
+// console.log(user1.username);
+// console.log(user1.password);
